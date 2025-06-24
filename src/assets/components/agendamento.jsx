8a75e8a29be66904692b6afa6adbc1c5065ca79a
gontijoVitor <vitorgontijo2017@gmail.com>
2025-06-24 19:37:48 -0300

    Backup

---
+++ src/assets/components/agendamento.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import ''
+
+function App() {
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,5 @@
-// 1. Importa o CSS do Bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-// 2. (Opcional) Importa os scripts JS do Bootstrap, caso use componentes como modal, collapse, etc.
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 import { StrictMode } from 'react'
