8b4e1e6301735b32fc60bbcc90bb08d36bf76c66
Vitor Gontijo <vitorgontijo2017@gmail.com>
2025-07-31 20:56:10 -0300

    update pequeno

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,9 @@
 import './index.css';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Agendamento from './pages/Agendamento.jsx'
+import Agendamento from './pages/cria_agendamento.jsx'
 
-import Horarios from './pages/horarios.jsx';
+import Horarios from './pages/lista_agendamentos.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
