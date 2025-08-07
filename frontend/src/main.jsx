bbf38aea47e0b29476e024a8cafcfbc174319487
Vitor Gontijo <vitorgontijo2017@gmail.com>
2025-08-06 21:24:56 -0300

    página de config

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Agendamento from './pages/cria_agendamento.jsx'
 
-import User_login from './pages/login_usuario.jsx';
-import User_register from './pages/cadastro_usuario.jsx';
+import User_login from './pages/user_login.jsx';
+import User_register from './pages/user_signin.jsx';
+import Config from './pages/configuracoes.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <User_login />
+    <Config />
   </StrictMode>,
 )
